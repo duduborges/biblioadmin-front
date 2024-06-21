b74db890fa65f7076703acb5a865227e31489799
JOÃO PAULO ARAUJO CAPPELETTI <joao_cappeletti@estudante.sesisenai.org.br>
2024-06-21 10:06:31 -0300

    saladasas

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,16 @@ function App() {
 
   return (
     <>
-      { <Navbar fluid rounded>
-      <Navbar.Brand href="https://flowbite-react.com">
-        <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
-      </Navbar.Brand>
-      <div className="flex md:order-2">
-        <Button>Get started</Button>
-        <Navbar.Toggle />
+    <div>
+      <h1 className='color-red'>SISTEMA BIBLIOTECA</h1>
+      <a className='tituloLogin'>Login</a>
+      <a>Usuário</a>
+      <div className='inputsLogin'>
+        <input type="text" className='inputUsuario'/>
+        <a>Senha</a>
+        <input type="text" className='inputSenha'/>
       </div>
-      <Navbar.Collapse>
-        <Navbar.Link href="#" active>
-          Home
-        </Navbar.Link>
-        <Navbar.Link href="#">About</Navbar.Link>
-        <Navbar.Link href="#">Services</Navbar.Link>
-        <Navbar.Link href="#">Pricing</Navbar.Link>
-        <Navbar.Link href="#">Contact</Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar> }
-    <div className='bg-black w-10 text-xl'>aaaaaaaaa</div>
+    </div>
     </>
   )
 }

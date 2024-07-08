8133443829f885224f14d96ee679943674c9eaa7
EDUARDO BORGES CAMBRAIA <eduardo_cambraia@estudante.sesisenai.org.br>
2024-07-08 08:10:47 -0300

    atualizado

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -24,62 +24,8 @@ export default function Home() {
     
     return (
         <>
-        <Nav />
-        {isblibliotecario? (
-            <>
-            <Navbar fluid rounded className='w-2/3 m-auto '>
-        <Navbar.Brand >
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">BiblioAdmin</span>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <div className="flex md:order-2">
-        {logado ? (
-            <>
-              <span className="mx-4 self-center whitespace-nowrap text-sm font-semibold dark:text-white">Bem vindo {nome}</span>
-              <button className="py-2 px-5 font- rounded-lg text-white font-bold bg-red-600 hover:bg-red-400" onClick={deletar} >Sair</button>
-            </>
-            ) : (
-              <>
-              <button className="py-2 px-5 font- rounded-lg hover:text-white font-bold hover:scale-110 hover:transition-all bg-green-400 text-black" onClick={() => navigate("/login")} >Login</button>
-            </>
-          )}
-          <Navbar.Toggle />
-        </div>
-        <Navbar.Collapse>
-          <Navbar.Link href="#">Emprestimos</Navbar.Link>
-          <Navbar.Link href="#">Alunos</Navbar.Link>
-          <Navbar.Link href="#">Livros</Navbar.Link>
-        </Navbar.Collapse>
-      </Navbar>
-            </>
-        ) :(
-            <>
-             <Navbar fluid rounded className='w-2/3 m-auto '>
-        <Navbar.Brand >
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">BiblioAdmin</span>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <div className="flex md:order-2">
-        {logado ? (
-            <>
-              <span className="mx-4 self-center whitespace-nowrap text-sm font-semibold dark:text-white">Bem vindo {nome}</span>
-              <button className="py-2 px-5 font- rounded-lg text-white font-bold bg-red-600 hover:bg-red-400" onClick={deletar} >Sair</button>
-            </>
-            ) : (
-              <>
-              <button className="py-2 px-5 font- rounded-lg hover:text-white font-bold hover:scale-110 hover:transition-all bg-green-400 text-black" onClick={() => navigate("/login")} >Login</button>
-            </>
-          )}
-          <Navbar.Toggle />
-        </div>
-        <Navbar.Collapse>
-          <Navbar.Link href="#">Emprestimos</Navbar.Link>
-          <Navbar.Link href="#">Livros</Navbar.Link>
-        </Navbar.Collapse>
-      </Navbar>
-            </>
-        ) }
+          <Nav/>
         </>
-
+      
     )
 }
